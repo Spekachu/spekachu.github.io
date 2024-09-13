8bddbd12a9667a3c7f8b85636890ae4d51f16f04
Spencer Johnson <Spekachu@users.noreply.github.com>
2024-09-13 17:51:12 +0200

    certs

--- src/components/EducationPage.js
+++ src/components/EducationPage.js
@@ -65,7 +65,56 @@ const EducationPage = () => {
             </a>
           </p>
         </div>
-        
+        <div className="my-12">
+					<p className="text-xl mb-6">
+						Over the years, I have pursued self-education through various online platforms, including FreeCodeCamp and The Odin Project. Below are some of the certifications I've earned along the way:
+					</p>
+
+					<div className="certs-wrapper flex flex-wrap gap-6 justify-center">
+						{/* Certificate 1 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://learn.codesignal.com/certificates/cm0ezx2je001khgzalxcunriy/course-paths/10" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/algo-certificate.png" alt="Mastering Algorithms and Data Structures in JavaScript" className="w-64 h-auto" />
+							</a>
+						</div>
+
+						{/* Certificate 2 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://www.sololearn.com/certificates/CT-7PBKJV6U" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/html-cert.png" alt="HTML Certificate" className="w-64 h-auto" />
+							</a>
+						</div>
+
+						{/* Certificate 3 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://www.sololearn.com/certificates/CT-OZBC2TAM" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/css-cert.png" alt="CSS Certificate" className="w-64 h-auto" />
+							</a>
+						</div>
+
+						{/* Certificate 4 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://www.sololearn.com/certificates/CT-LPFFZWAK" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/js-cert.png" alt="JS Certificate" className="w-64 h-auto" />
+							</a>
+						</div>
+
+						{/* Certificate 5 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://www.sololearn.com/certificates/CT-6W7K6X93" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/php-cert.png" alt="PHP Certificate" className="w-64 h-auto" />
+							</a>
+						</div>
+
+						{/* Certificate 6 */}
+						<div className="cert shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-lg overflow-hidden">
+							<a href="https://www.sololearn.com/certificates/CT-0IXCNSSN" target="_blank" rel="noopener noreferrer">
+								<img src="/assets/sql-cert.png" alt="SQL Certificate" className="w-64 h-auto" />
+							</a>
+						</div>
+					</div>
+				</div>
+
       </div>
     </div>
   );
